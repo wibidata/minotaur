f802c40166fb90efe0e098c243a4811aecd5eb64
Space <space@wibidata.com>
2015-04-15 15:13:54 -0700

    added java8 attributes and log_dir for zookeeper

--- labs/zookeeper/chef/site-cookbooks/zookeeper/attributes/default.rb
+++ labs/zookeeper/chef/site-cookbooks/zookeeper/attributes/default.rb
@@ -8,9 +8,15 @@ default[:zookeeper][:user]        = 'zookeeper'
 
 default[:zookeeper][:install_dir] = '/opt/apache/zookeeper'
 default[:zookeeper][:data_dir] = '/var/lib/zookeeper'
+default[:zookeeper][:log_dir] = '/var/log/zookeeper'
 
 default[:zookeeper][:id] = '0'
 default[:zookeeper][:peer_port] = '2888'
 default[:zookeeper][:leader_port] = '3888'
 
 default[:zookeeper][:servers] = nil
+
+default[:java][:jdk_version] = 8
+default[:java][:install_flavor] = "oracle"
+default[:java][:oracle][:accept_oracle_download_terms] = true
+default[:java][:remove_deprecated_packages] = true
